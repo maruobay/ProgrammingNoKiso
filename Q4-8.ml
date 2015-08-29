d48181d9d1852fa0dac2498aaabc3b4e9ac471f1
maruobay <maruobay@gmail.com>
2015-08-29 14:41:38 +0900

    p.31

---
+++ Q4-8.ml
@@ -0,0 +1,28 @@
+問題4.8
+(1)
+目的：鶴と亀の数の合計と足の数の合計を与えたら、鶴の数を返す
+型：int -> int -> int
+
+(2)
+(* 目的：鶴と亀の数の合計と足の数の合計を与えたら、鶴の数を返す *)
+(* int -> int -> int *)
+
+(3)
+tsurukame 2 6 = 1
+tsurukame 3 8 = 2
+tsurukame 4 12 = 2
+
+(4)
+let test1 = tsurukame 2 6 = 1
+let test2 = tsurukame 3 8 = 2
+let test3 = tsurukame 4 12 = 2
+
+(5)
+let tsurukame tsurukame_no_kazu ashi_no_kazu
+= (4*tsurukame_no_kazu - ashi_no_kazu)/2
+
+(6)
+略
+
+(7)
+略
