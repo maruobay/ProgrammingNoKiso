296103d0b5e3d771daf873473a99d0f7f86eb7da
maruobay <maruobay@gmail.com>
2015-08-30 17:50:26 +0900

    p.49

---
+++ Q6-1.ml
@@ -0,0 +1,4 @@
+(* 問題6.1 *)
+(1) intであるべき引数がfloatになっている -> Error: This expression has type float but an expression was of type int
+(2) piとrがunbound -> Error: Unbound value pi
+(3) TVは大文字なのでダメ -> Error: Unbound constructor VD
